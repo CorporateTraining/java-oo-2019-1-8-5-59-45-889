16a3ed91691754d59c69ddd0bbad7e35e5f1fee4
mahuanfy <mahuanfy@foxmail.com>
2019-04-03 14:13:11 +0800

    Finish practice 09 test

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,39 @@
 package practice09;
 
-public class Teacher {
+import java.util.Objects;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        if (Objects.isNull(this.klass)) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class " + this.klass.getNumber() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        String result = super.introduce() + " I am a Teacher.";
+        if(this.klass.getNumber() == student.getKlass().getNumber()){
+            return result + " I teach Jerry.";
+        }
+        return result + " I don't teach Jerry.";
+    }
 }
