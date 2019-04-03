36fd440d5f5ec392659223b057d7cc9920cfc086
mahuanfy <mahuanfy@foxmail.com>
2019-04-03 14:00:53 +0800

    Finish practice 08 test

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,27 @@
 package practice08;
 
-public class Student {
+import java.util.Objects;
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        if (Objects.nonNull(this.klass.getLeader()) && this.klass.getLeader().getId() == this.getId()) {
+            return super.introduce() + " I am a Student. I am Leader of Class " + this.klass.getNumber() + ".";
+        }
+        return super.introduce() + " I am a Student. I am at Class " + this.klass.getNumber() + ".";
+    }
 }
